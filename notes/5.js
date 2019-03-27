bb92a1bfc13558c0364da91f6fea5fbe555a3ba3
Kevin <farthing.kw@gmail.com>
2019-03-26 23:28:23 -0500

    lecture 54

--- notes/5.js
+++ notes/5.js
@@ -15,3 +15,10 @@
 
 // so we're committing the config file because it's using variables (formatted as SUBJECT_USE ie GOOGLE_URI)
 // and we set the config vars on heroku (project, setting, config vars)
+
+// uri mismatch due to http vs https
+// ah, googlestrategy relative path only has '/auth/google/callback'
+// so it's assuming http://whatever.com/auth/google/callback, instead of https.
+// also because heroku
+// because heroku has a proxy
+// which tells google to automatically not treat it as secure
