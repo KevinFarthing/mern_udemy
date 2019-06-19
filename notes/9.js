4a798ab4e5d82d3ee0da5dc18721662ef7714ad2
Kevin <farthing.kw@gmail.com>
2019-06-19 01:28:11 -0500

    116

--- notes/9.js
+++ notes/9.js
@@ -5,3 +5,10 @@
 // prod
 //     bundle.js -> public assets
 //     api data ->node/express
+
+//  push to heroku -> tell heroku to install all deps -> heroku builds client project
+//  push to CI -> run tests and shit -> CI builds and commits client -> CI pushes build to heroku
+
+// so both the main app and the client app have a package.json
+// BUT heroku will hit the main package.json, and never care about the client one
+// so all the goods have to be present in main
